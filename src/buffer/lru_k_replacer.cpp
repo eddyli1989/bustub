85b8313cd26d9415d22139eed1bec9ba9cfd0526
eddyli <eddyli@outlook.com>
2023-01-30 08:05:43 +0800

    实现LRU-K

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -28,34 +28,103 @@ auto LRUKNode::GetFid() -> frame_id_t {
 }
 
 auto LRUKNode::GetBackwardKDist(size_t current_timestamp) -> size_t {
-  throw NotImplementedException("GetBackwardKDist not implemented");
-  return 0;
+  if (HasInfBackwardKDist()) return -1;
+  return current_timestamp - (history_.back());
 }
 
 auto LRUKNode::HasInfBackwardKDist() -> bool {
-  throw NotImplementedException("HasInfBackwardKDist not implemented");
-  return false;
+  return history_.size() <= k_;
 }
 
 auto LRUKNode::GetEarliestTimestamp() -> size_t {
-  throw NotImplementedException("GetEarliestTimestamp not implemented");
-  return 0;
+  if (history_.size() == 0) return 0;
+  return *history_.begin();
 }
 
 void LRUKNode::InsertHistoryTimestamp(size_t current_timestamp) {
-  throw NotImplementedException("InsertHistoryTimestamp not implemented");
+    if (history_.size() >= k_) {
+        history_.pop_front(); //todo? update the last or pop front??
+    }
+    history_.push_back(current_timestamp);
 }
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool 
+{ 
+    size_t max_dist = 0;
+    bool has_inf = false;
+    size_t inf_max = 0;
+    frame_id_t evict_frame_id = -1;
+    current_timestamp_ = std::time(0);
+    for (auto & node : node_store_) {
+        if (!node.second.IsEvictable()) continue;
+        if (node.second.HasInfBackwardKDist()) {
+            has_inf = true;
+            auto dist = current_timestamp_ - node.second.GetEarliestTimestamp(); // todo??
+            if (dist > inf_max) {
+                inf_max = dist;
+                evict_frame_id = node.first;
+            }
+            continue;
+        }
+        if (has_inf) continue;
+        auto dist = node.second.GetBackwardKDist(current_timestamp_);
+        if (dist > max_dist) {
+            max_dist = dist;
+            evict_frame_id = node.first;
+        }
+    }
+    if (evict_frame_id != -1) {
+        Remove(evict_frame_id);
+        return true;
+    }
+    return false; 
+}
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) 
+{
+    BUSTUB_ASSERT(frame_id < replacer_size_, "invalid frame_id");
+    auto it = node_store_.find(frame_id);
+    if (it == node_store_.end()) {
+        if (node_store_.size() >= replacer_size_) {
+            frame_id_t evict_frame;
+            auto evict = Evict(&evict_frame);
+            if (!evict) return;
+        }
+        LRUKNode node(k_, frame_id);
+        node.InsertHistoryTimestamp(current_timestamp_);
+        node_store_[frame_id] = node;
+        curr_size_++;
+        return;
+    }
+    it->second.InsertHistoryTimestamp(current_timestamp_);
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) 
+{
+  auto it = node_store_.find(frame_id);
+  if(it == node_store_.end()) {
+    return;
+  }
+  if (!it->second.IsEvictable() && set_evictable) curr_size_++;
+  if (it->second.IsEvictable() && !set_evictable) curr_size_--;
+  it->second.SetEvictable(set_evictable);
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::Remove(frame_id_t frame_id) 
+{
+    BUSTUB_ASSERT(frame_id < replacer_size_, "invalid frame_id");
+    //BUSTUB_ASSERT(curr_size_ > 0, "invalid size when remove");
+    auto it = node_store_.find(frame_id);
+    if(it == node_store_.end()) {
+        return;
+    }
+    BUSTUB_ASSERT(it->second.IsEvictable(), "Not Evictable when remove");
+    node_store_.erase(it);
+    curr_size_--;
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
