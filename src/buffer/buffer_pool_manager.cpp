4c30539c5e5bc18a5fc72203e037fab505007da3
eddyli <eddyli@outlook.com>
2023-02-06 07:28:07 +0800

    finish bufferpool

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -22,7 +22,6 @@ namespace bustub {
 BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager, size_t replacer_k,
                                      LogManager *log_manager)
     : pool_size_(pool_size), disk_manager_(disk_manager), log_manager_(log_manager) {
-
   // we allocate a consecutive memory space for the buffer pool
   pages_ = new Page[pool_size_];
   replacer_ = std::make_unique<LRUKReplacer>(pool_size, replacer_k);
@@ -35,158 +34,156 @@ BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager
 
 BufferPoolManager::~BufferPoolManager() { delete[] pages_; }
 
-auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * { 
-    const std::lock_guard<std::mutex> lock(latch_);
-    frame_id_t free_frame_id = -1;
-    if (free_list_.empty()) {
-        auto evcit_ret = replacer_->Evict(&free_frame_id);
-        if (!evcit_ret) return nullptr;
-        LOG_DEBUG("free_list_ is empty, evcit:%d",  free_frame_id);
-    } else {
-        free_frame_id = free_list_.front();
-        free_list_.pop_front();
-        LOG_DEBUG("free_list_ is not empty, size: %d, free_frame_id:%d", free_list_.size(), free_frame_id);
-    }
-    BUSTUB_ASSERT(free_frame_id >=0 && free_frame_id < (int)pool_size_, "invalid frame_id");
-   
-    Page* page = &pages_[free_frame_id];
-    if (page->IsDirty()) {
-        LOG_DEBUG("page is dirty, write first, frame_id:%d", free_frame_id);
-        FlushPage(page->GetPageId());
-    }
+auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
+  const std::lock_guard<std::mutex> lock(latch_);
+  frame_id_t free_frame_id = -1;
+  if (free_list_.empty()) {
+    auto evcit_ret = replacer_->Evict(&free_frame_id);
+    if (!evcit_ret) return nullptr;
+    LOG_DEBUG("free_list_ is empty, evcit:%d", free_frame_id);
+  } else {
+    free_frame_id = free_list_.front();
+    free_list_.pop_front();
+    LOG_DEBUG("free_list_ is not empty, size: %lu, free_frame_id:%d", free_list_.size(), free_frame_id);
+  }
+  BUSTUB_ASSERT(free_frame_id >= 0 && free_frame_id < (int)pool_size_, "invalid frame_id");
 
-    if (page->GetPageId() != INVALID_PAGE_ID) {
-        page_table_.erase(page->GetPageId());
-    }
-    
-    page->ResetMemory();
-    auto allocated_page_id = AllocatePage();
-    LOG_DEBUG("Allocated page_id:%d",  allocated_page_id);
-    page->page_id_ = allocated_page_id;
-    page->pin_count_ = 1; // todo ?
-    page->is_dirty_ = false;
-    page_table_[allocated_page_id] = free_frame_id;
-    *page_id = allocated_page_id;
-    replacer_->RecordAccess(free_frame_id);
-    replacer_->SetEvictable(free_frame_id, false);
-    return page;
+  Page *page = &pages_[free_frame_id];
+  if (page->IsDirty()) {
+    LOG_DEBUG("page is dirty, write first, frame_id:%d", free_frame_id);
+    latch_.unlock();
+    FlushPage(page->GetPageId());
+    latch_.lock();
+  }
+
+  if (page->GetPageId() != INVALID_PAGE_ID) {
+    page_table_.erase(page->GetPageId());
+  }
+
+  page->ResetMemory();
+  auto allocated_page_id = AllocatePage();
+  LOG_DEBUG("Allocated page_id:%d", allocated_page_id);
+  page->page_id_ = allocated_page_id;
+  page->pin_count_ = 1;  // todo ?
+  page->is_dirty_ = false;
+  page_table_[allocated_page_id] = free_frame_id;
+  *page_id = allocated_page_id;
+  replacer_->RecordAccess(free_frame_id);
+  replacer_->SetEvictable(free_frame_id, false);
+  return page;
 }
 
-auto BufferPoolManager::FetchPage(page_id_t page_id) -> Page * { 
-    const std::lock_guard<std::mutex> lock(latch_);   
-    auto page = GetPage(page_id);
-    if (page != nullptr) return page;
-    // not in memory, read from disk
-    frame_id_t* free_frame_id = nullptr;
-  
-    if (free_list_.empty()) {
-        auto evcit_ret = replacer_->Evict(free_frame_id);
-        if (!evcit_ret) return nullptr;
-    } else {
-        free_frame_id = &free_list_.front();
-        free_list_.pop_front();
-    }
+auto BufferPoolManager::FetchPage(page_id_t page_id) -> Page * {
+  const std::lock_guard<std::mutex> lock(latch_);
+  auto page = GetPage(page_id);
+  if (page != nullptr) return page;
+  // not in memory, read from disk
+  frame_id_t free_frame_id = -1;
 
-    page = &pages_[*free_frame_id];
-    if (page->IsDirty()) {
-        latch_.unlock();
-        FlushPage(page->GetPageId()); // todo dead lock? yes
-        latch_.lock();
-    }
+  if (free_list_.empty()) {
+    auto evcit_ret = replacer_->Evict(&free_frame_id);
+    if (!evcit_ret) return nullptr;
+  } else {
+    free_frame_id = free_list_.front();
+    free_list_.pop_front();
+  }
 
-    if (page->GetPageId() != INVALID_PAGE_ID) {
-        page_table_.erase(page->GetPageId());
-    }
-    page->page_id_ = page_id;
-    page->pin_count_ = 1; // todo ?
-    page->is_dirty_ = false;
-    page_table_[page_id] = *free_frame_id;
-    page->ResetMemory();
-    replacer_->RecordAccess(*free_frame_id);
-    replacer_->SetEvictable(*free_frame_id, false);
-    disk_manager_->ReadPage(page_id, page->GetData());
-    return page; 
+  page = &pages_[free_frame_id];
+  if (page->IsDirty()) {
+    latch_.unlock();
+    FlushPage(page->GetPageId());  // todo dead lock? yes
+    latch_.lock();
+  }
+
+  if (page->GetPageId() != INVALID_PAGE_ID) {
+    page_table_.erase(page->GetPageId());
+  }
+  page->page_id_ = page_id;
+  page->pin_count_ = 1;  // todo ?
+  page->is_dirty_ = false;
+  page_table_[page_id] = free_frame_id;
+  page->ResetMemory();
+  replacer_->RecordAccess(free_frame_id);
+  replacer_->SetEvictable(free_frame_id, false);
+  disk_manager_->ReadPage(page_id, page->GetData());
+  return page;
 }
 
 auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty) -> bool {
-    const std::lock_guard<std::mutex> lock(latch_);
-    auto page = GetPage(page_id);
-    if (page == nullptr) return false;
-    if (page->GetPinCount() == 0) return false;
-    page->pin_count_--;
-    page->is_dirty_ = is_dirty;
-    if (page->GetPinCount() == 0) {
-        replacer_->SetEvictable(page_table_[page_id], true);
-    }
-    return true; 
+  const std::lock_guard<std::mutex> lock(latch_);
+  auto page = GetPage(page_id);
+  if (page == nullptr) return false;
+  if (page->GetPinCount() == 0) return false;
+  page->pin_count_--;
+  page->is_dirty_ = is_dirty;
+  if (page->GetPinCount() == 0) {
+    replacer_->SetEvictable(page_table_[page_id], true);
+  }
+  return true;
 }
 
-Page* BufferPoolManager::GetPage(page_id_t page_id) {
-    if (page_id == INVALID_PAGE_ID) return nullptr;
-    auto it = page_table_.find(page_id);
-    if (it == page_table_.end()) return nullptr;
-    return &pages_[it->second];
+Page *BufferPoolManager::GetPage(page_id_t page_id) {
+  if (page_id == INVALID_PAGE_ID) return nullptr;
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) return nullptr;
+  return &pages_[it->second];
 }
 
 auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool {
-    const std::lock_guard<std::mutex> lock(latch_);
-    Page* page = GetPage(page_id);
-    if (page == nullptr) return false;
-    disk_manager_->WritePage(page_id, page->GetData());
-    page->is_dirty_ = false;
-    return true; 
+  const std::lock_guard<std::mutex> lock(latch_);
+  Page *page = GetPage(page_id);
+  if (page == nullptr) return false;
+  disk_manager_->WritePage(page_id, page->GetData());
+  page->is_dirty_ = false;
+  return true;
 }
 
 void BufferPoolManager::FlushAllPages() {
-    for (auto &it : page_table_) {
-        FlushPage(it.first);
-    }
+  for (auto &it : page_table_) {
+    FlushPage(it.first);
+  }
 }
 
-auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool { 
-    const std::lock_guard<std::mutex> lock(latch_);
-    auto page = GetPage(page_id);
-    if (page == nullptr) return true;
-    if (page->GetPinCount() != 0) return false;
-    if (page->IsDirty()) {
-        latch_.unlock();
-        FlushPage(page_id);
-        latch_.lock();
-    }
-    auto frame_id = page_table_[page_id];
-    page_table_.erase(page_id);
-    replacer_->Remove(frame_id);
-    page->ResetMemory();
-    page->page_id_ = INVALID_PAGE_ID;
-    free_list_.push_back(frame_id);
-    DeallocatePage(page_id);
-    return true;
+auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool {
+  const std::lock_guard<std::mutex> lock(latch_);
+  auto page = GetPage(page_id);
+  if (page == nullptr) return true;
+  if (page->GetPinCount() != 0) return false;
+  if (page->IsDirty()) {
+    latch_.unlock();
+    FlushPage(page_id);
+    latch_.lock();
+  }
+  auto frame_id = page_table_[page_id];
+  page_table_.erase(page_id);
+  replacer_->Remove(frame_id);
+  page->ResetMemory();
+  page->page_id_ = INVALID_PAGE_ID;
+  free_list_.push_back(frame_id);
+  DeallocatePage(page_id);
+  return true;
 }
 
 auto BufferPoolManager::AllocatePage() -> page_id_t { return next_page_id_++; }
 
-auto BufferPoolManager::FetchPageBasic(page_id_t page_id) -> BasicPageGuard { 
-    return {this, FetchPage(page_id)}; 
+auto BufferPoolManager::FetchPageBasic(page_id_t page_id) -> BasicPageGuard { return {this, FetchPage(page_id)}; }
+
+auto BufferPoolManager::FetchPageRead(page_id_t page_id) -> ReadPageGuard {
+  auto page = FetchPage(page_id);
+  if (page != nullptr) {
+    page->RLatch();
+  }
+  return {this, page};
 }
 
-auto BufferPoolManager::FetchPageRead(page_id_t page_id) -> ReadPageGuard { 
-    auto page = FetchPage(page_id);
-    if (page != nullptr) {
-        page->RLatch();
-    }
-    return {this, page}; 
+auto BufferPoolManager::FetchPageWrite(page_id_t page_id) -> WritePageGuard {
+  auto page = FetchPage(page_id);
+  if (page != nullptr) {
+    page->WLatch();
+  }
+  return {this, nullptr};
 }
 
-auto BufferPoolManager::FetchPageWrite(page_id_t page_id) -> WritePageGuard { 
-    auto page = FetchPage(page_id);
-    if (page != nullptr) {
-        page->WLatch();
-    }
-    return {this, nullptr}; 
-}
-
-auto BufferPoolManager::NewPageGuarded(page_id_t *page_id) -> BasicPageGuard { 
-    return {this, NewPage(page_id)}; 
-}
+auto BufferPoolManager::NewPageGuarded(page_id_t *page_id) -> BasicPageGuard { return {this, NewPage(page_id)}; }
 
 }  // namespace bustub
--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -31,95 +31,95 @@ auto LRUKNode::GetBackwardKDist(size_t current_timestamp) -> size_t {
 auto LRUKNode::HasInfBackwardKDist() -> bool { return history_.size() < k_; }
 
 auto LRUKNode::GetEarliestTimestamp() -> size_t {
-    if (history_.size() == 0) return 0;
-    return *history_.begin();
+  if (history_.size() == 0) return 0;
+  return *history_.begin();
 }
 
 void LRUKNode::InsertHistoryTimestamp(size_t current_timestamp) {
-    if (history_.size() >= k_) {
-        history_.pop_front();  //  todo? update the last or pop front??
-    }
-    history_.push_back(current_timestamp);
+  if (history_.size() >= k_) {
+    history_.pop_front();  //  todo? update the last or pop front??
+  }
+  history_.push_back(current_timestamp);
 }
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
-//  todo lock
+
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
-    size_t max_dist = 0;
-    bool has_inf = false;
-    size_t inf_max = 0;
-    frame_id_t evict_frame_id = -1;
-    
-    const std::lock_guard<std::mutex> lock(latch_);
-    for (auto &node : node_store_) {
-        if (!node.second.IsEvictable()) continue;
-        if (node.second.HasInfBackwardKDist()) {
-            has_inf = true;
-            auto dist = current_timestamp_ - node.second.GetEarliestTimestamp();  // todo??
-            if (dist >= inf_max) {
-                inf_max = dist;
-                evict_frame_id = node.first;
-            }
-            continue;
-        }
-        if (has_inf) continue;
-        auto dist = node.second.GetBackwardKDist(current_timestamp_);
-        if (dist >= max_dist) {
-            max_dist = dist;
-            evict_frame_id = node.first;
-        }
+  size_t max_dist = 0;
+  bool has_inf = false;
+  size_t inf_max = 0;
+  frame_id_t evict_frame_id = -1;
+
+  const std::lock_guard<std::mutex> lock(latch_);
+  for (auto &node : node_store_) {
+    if (!node.second.IsEvictable()) continue;
+    if (node.second.HasInfBackwardKDist()) {
+      has_inf = true;
+      auto dist = current_timestamp_ - node.second.GetEarliestTimestamp();  // todo??
+      if (dist >= inf_max) {
+        inf_max = dist;
+        evict_frame_id = node.first;
+      }
+      continue;
     }
-    if (evict_frame_id != -1) {
-        *frame_id = evict_frame_id;
-        latch_.unlock();
-        Remove(evict_frame_id);
-        latch_.lock();
-        return true;
+    if (has_inf) continue;
+    auto dist = node.second.GetBackwardKDist(current_timestamp_);
+    if (dist >= max_dist) {
+      max_dist = dist;
+      evict_frame_id = node.first;
     }
-    return false;
+  }
+  if (evict_frame_id != -1) {
+    *frame_id = evict_frame_id;
+    latch_.unlock();
+    Remove(evict_frame_id);
+    latch_.lock();
+    return true;
+  }
+  return false;
 }
 
 void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
-    BUSTUB_ASSERT(static_cast<size_t>(frame_id) < replacer_size_, "invalid frame_id");
-    const std::lock_guard<std::mutex> lock(latch_);
-    auto it = node_store_.find(frame_id);
-    if (it == node_store_.end()) {
-        if (node_store_.size() >= replacer_size_) {
-        frame_id_t evict_frame;
-        latch_.unlock();
-        auto evict = Evict(&evict_frame);
-        latch_.lock();
-        if (!evict) return;
-        }
-        LRUKNode node(k_, frame_id);
-        node.InsertHistoryTimestamp(current_timestamp_++);
-        node_store_.insert(std::make_pair(frame_id, node));
-        return;
+  BUSTUB_ASSERT(static_cast<size_t>(frame_id) < replacer_size_, "invalid frame_id");
+  const std::lock_guard<std::mutex> lock(latch_);
+  auto it = node_store_.find(frame_id);
+  if (it == node_store_.end()) {
+    if (node_store_.size() >= replacer_size_) {
+      frame_id_t evict_frame;
+      latch_.unlock();
+      auto evict = Evict(&evict_frame);
+      latch_.lock();
+      if (!evict) return;
     }
-    it->second.InsertHistoryTimestamp(current_timestamp_++);
+    LRUKNode node(k_, frame_id);
+    node.InsertHistoryTimestamp(current_timestamp_++);
+    node_store_.insert(std::make_pair(frame_id, node));
+    return;
+  }
+  it->second.InsertHistoryTimestamp(current_timestamp_++);
 }
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
-    const std::lock_guard<std::mutex> lock(latch_); 
-    auto it = node_store_.find(frame_id);
-    if (it == node_store_.end()) {
-        return;
-    }
-    if (!it->second.IsEvictable() && set_evictable) curr_size_++;
-    if (it->second.IsEvictable() && !set_evictable) curr_size_--;
-    it->second.SetEvictable(set_evictable);
+  const std::lock_guard<std::mutex> lock(latch_);
+  auto it = node_store_.find(frame_id);
+  if (it == node_store_.end()) {
+    return;
+  }
+  if (!it->second.IsEvictable() && set_evictable) curr_size_++;
+  if (it->second.IsEvictable() && !set_evictable) curr_size_--;
+  it->second.SetEvictable(set_evictable);
 }
 
 void LRUKReplacer::Remove(frame_id_t frame_id) {
-    BUSTUB_ASSERT(static_cast<size_t>(frame_id) < replacer_size_, "invalid frame_id");
-    const std::lock_guard<std::mutex> lock(latch_); 
-    auto it = node_store_.find(frame_id);
-    if (it == node_store_.end()) {
-        return;
-    }
-    BUSTUB_ASSERT(it->second.IsEvictable(), "Not Evictable when remove");
-    node_store_.erase(it);
-    curr_size_--;
+  BUSTUB_ASSERT(static_cast<size_t>(frame_id) < replacer_size_, "invalid frame_id");
+  const std::lock_guard<std::mutex> lock(latch_);
+  auto it = node_store_.find(frame_id);
+  if (it == node_store_.end()) {
+    return;
+  }
+  BUSTUB_ASSERT(it->second.IsEvictable(), "Not Evictable when remove");
+  node_store_.erase(it);
+  curr_size_--;
 }
 
 auto LRUKReplacer::Size() -> size_t { return curr_size_; }
